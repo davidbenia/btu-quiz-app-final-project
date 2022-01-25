255234a320454c9f0ad64435febea971a69fa5ec
davidbenia <david.benia.1@btu.edu.ge>
2022-01-25 12:33:54 +0400

    Quiz Skeleton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { Routes, Route, Link, Navigate } from "react-router-dom";
+import Quiz from "./components/Quiz";
 
 function App() {
   return (
     <div className="App">
       <div className="Header">
-        <nav className="Links">;</nav>
+        <nav className="Links">
+          <Link className="" to="/quiz">
+            Quiz
+          </Link>
+        </nav>
       </div>
 
       <div className="Content">
-        <Routes>;</Routes>
+        <Routes>
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
       </div>
     </div>
   );
