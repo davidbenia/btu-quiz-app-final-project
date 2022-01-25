1e97c199cce94d5666e9b678f834ef950724312b
davidbenia <david.benia.1@btu.edu.ge>
2022-01-25 12:19:42 +0400

    Third Commit - Added routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <></>
+      <div className="Header">
+        <nav className="Links">;</nav>
+      </div>
+
+      <div className="Content">
+        <Routes>;</Routes>
+      </div>
     </div>
   );
 }
