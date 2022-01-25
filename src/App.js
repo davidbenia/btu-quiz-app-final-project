9c5926eaadfb310661dd4c6fdd2b12162eed856a
davidbenia <david.benia.1@btu.edu.ge>
2022-01-25 16:48:13 +0400

    Added styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { Routes, Route, Link, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div className="Header">
-        <nav className="Links">;</nav>
+    <div className="App absolute w-full h-full bg-gray-200 flex justify-center">
+      <div className="Header bg-gray-300 mt-5 w-4/5 h-24 rounded-lg flex items-center justify-center">
+        <nav className="Links flex gap-x-5 py-2 px-4 text-white"></nav>
       </div>
 
       <div className="Content">
